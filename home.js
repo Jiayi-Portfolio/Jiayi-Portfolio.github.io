54bc188f19054b4c492a67b1edbd13b328ce5851
JiayiLi18 <jiayi.li0004@gmail.com>
2024-02-21 21:01:39 +0200

    add project4 and 5

--- home.js
+++ home.js
@@ -5,16 +5,24 @@ var images=[
     "url(images/fungiCity_cover.png)",
     "url(images/cat_impact_cover.png)",
     "url(images/crossing_liminal_cover.png)",
+    "url(images/face_cover.png)",
+    "url(images/nora_cover.jpg)",
+    "url(images/odyssey_cover.png)",
+    "url(images/nora_cover.jpg)",
+    "url(images/ItsGoingToBeLate_cover.png)",
 ]
 
 var links=[
     "project_1.html",
     "project_2.html",
-    "project_1.html",
-    "project_2.html",
+    "project_4.html",
+    "project_3.html",
     "https://lijiayii.itch.io/cat-impact",
     "https://itch.io/jam/games-now-jam-autumn2023/rate/2363964",
-
+    "https://newmedia.dog/courses/audiovisual-studio/2023/jiayi-li/",
+    "https://youtu.be/yCFVTMXcpxM?si=J8U6TcDyNoD9lz5P",
+    "https://lijiayii.itch.io/gravity-odyssey",
+    "https://readymag.website/u3749671887/3946875/otherworks/",
 ]
 
 var randomImage=document.getElementById('randomImage');
