18ffd6ffb7e2c1297da6814e3d7165d8ec3ac3d5
JiayiLi18 <jiayi.li0004@gmail.com>
2025-03-13 01:16:44 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  basePath: '/Portfolio',
-  // 在生产环境下，静态资源的前缀也要设置成相同的路径
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/Portfolio/' : '',
   output: 'export',
   //images: { unoptimized: true } 
 }
